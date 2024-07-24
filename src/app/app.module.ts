85ca19125a1b9646f59cfdedbe90bb0e54118ce3
anme-22 <angeldavidmendez2012@gmail.com>
2024-07-24 16:24:08 -0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import mapboxgl from 'mapbox-gl';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapModule } from './maps/map.module';
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 
@@ -7,11 +7,15 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrl: './full-screen-page.component.css',
 })
 export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
     const map = new mapboxgl.Map({
       accessToken:
         'pk.eyJ1IjoiYW5tZTIyIiwiYSI6ImNseXl1YmY5YTJnbzQyaXE3MTR3N3V1amYifQ.uFQsLLljZ1gCmCKB1V0MiA',
-      container: 'map', // container ID
+      container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-87.20681, 14.0818], // starting position [lng, lat]
       zoom: 9, // starting zoom
